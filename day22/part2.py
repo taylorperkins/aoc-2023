1c457bf6ff7ac6515e6e575f1d5f32d53430f9fd
Taylor Perkins <taylorperkins@iheartmedia.com>
2023-12-22 21:45:25 -0600

    22.2 -> 453.059ms

---
+++ day22/part2.py
@@ -0,0 +1,150 @@
+from __future__ import annotations
+
+import functools
+from collections import defaultdict
+from dataclasses import dataclass, field
+from itertools import chain
+from typing import Set, Dict, List, Any
+
+from utils import getInput, timeit
+
+
+def ranges_intersect(l: range, r: range):
+    # has to be a better way, but idk
+    return (
+        l.start <= r.start <= l.stop
+        or l.start <= r.stop <= l.stop
+        or r.start <= l.start <= r.stop
+        or r.start <= l.stop <= r.stop
+    )
+
+
+@dataclass
+class Brick:
+    id: Any
+    xr: range
+    yr: range
+    zr: range
+
+    parents: Dict[Any, Brick] = field(default_factory=dict)
+    children: Dict[Any, Brick] = field(default_factory=dict)
+
+    def add_parent(self, other: Brick):
+        self.parents[other.id] = other
+
+    def add_child(self, other: Brick):
+        self.children[other.id] = other
+
+    @property
+    def bottom(self):
+        return self.zr.start
+
+    @property
+    def top(self):
+        return self.zr.stop
+
+    def xy_intersect(self, other: Brick):
+        return ranges_intersect(self.xr, other.xr) and ranges_intersect(self.yr, other.yr)
+
+    def z_shift(self, n: int):
+        self.zr = range(self.zr.start + n, self.zr.stop + n)
+
+    @property
+    def hash(self):
+        return hash((self.xr, self.yr, self.zr,))
+
+    def __eq__(self, other: Brick):
+        return self.hash == other.hash
+
+    def __hash__(self):
+        return self.hash
+
+
+def disentegration_scores(bricks: Dict[int, Brick]) -> int:
+    def fn(current_bricks: Set[int], falling_bricks: Set[int], score: int) -> int:
+        if not current_bricks:
+            return score
+
+        affected_bricks = set()
+        for bid in current_bricks:
+            brick = bricks[bid]
+            for cid, child in brick.children.items():
+                if cid not in falling_bricks | affected_bricks and len(set(child.parents) - falling_bricks) == 0:
+                    affected_bricks.add(cid)
+                    score += 1
+
+        return fn(affected_bricks, falling_bricks | affected_bricks, score)
+
+    total = 0
+    for bid in bricks.keys():
+        total += fn({bid}, {bid}, score=0)
+    return total
+
+
+@timeit
+def main(aoc: str):
+    z_bricks: Dict[int, Dict[Any, Brick]] = defaultdict(dict)
+    bricks = {}
+
+    for (idx, line) in enumerate(aoc.splitlines()):
+        l, r = line.split("~")
+        l, r = l.split(","), r.split(",")
+        xr = range(*sorted([int(l[0]), int(r[0])]))
+        yr = range(*sorted([int(l[1]), int(r[1])]))
+        zr = range(*sorted([int(l[2]), int(r[2])]))
+
+        b = Brick(idx, xr, yr, zr)
+        bricks[idx] = b
+        for i in range(zr.start, zr.stop+1):
+            z_bricks[i][b.id] = b
+
+    # no need to start on the bottom
+    for z in range(2, max(z_bricks.keys())+1):
+        current_brick_ids = [bid for bid, b in z_bricks[z].items() if b.bottom == z]
+        for b_id in current_brick_ids:
+            brick = z_bricks[z][b_id]
+
+            next_level = z
+            i = 0
+            while True:
+                i += 1
+                lower_z = z-i
+                if lower_z == 0:
+                    break
+
+                intersecting_bricks = (b for b in z_bricks[lower_z].values() if brick.xy_intersect(b))
+                try:
+                    # ran into another brick. Connect them, then go back up
+                    b = next(intersecting_bricks)
+                    b.add_child(brick)
+                    brick.add_parent(b)
+
+                    for b in intersecting_bricks:
+                        b.add_child(brick)
+                        brick.add_parent(b)
+
+                    break
+
+                # no bricks, can shift down
+                except StopIteration:
+                    next_level = lower_z
+                    pass
+
+            shift = brick.bottom - next_level
+            if not shift:
+                continue
+
+            for bz in range(brick.zr.start, brick.zr.stop + 1):
+                del z_bricks[bz][brick.id]
+
+            brick.z_shift(-shift)
+
+            for bz in range(brick.zr.start, brick.zr.stop + 1):
+                z_bricks[bz][brick.id] = brick
+
+    print(disentegration_scores(bricks))
+
+
+if __name__ == "__main__":
+    main(getInput("./input-test.txt"))
+    main(getInput("./input.txt"))
